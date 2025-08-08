2088f82e1b33a0518c3b637c8dd47881ef023601
Himanshu-Jaiswar <dilipjaiswar3323@gmail.com>
2025-08-08 13:20:58 +0530

    Room-Chat Backend

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET", "POST"],
+    }
+});
+
+const users = new Set();
+
+io.on('connection', (socket) => {
+    console.log("id:",socket.id);
+    
+   
+    socket.on("join-room", (room) => {
+        users.add(socket.id);
+        console.log(users.size);
+        socket.join(room);
+        console.log(socket.rooms);
+        
+    });
+
+    socket.on("send-message", ({room, userMessage, sender, otherName}) => {
+        console.log(otherName);
+        
+        io.to(room).emit("receive-message", {userMessage, sender, otherName});
+    })
+
+    socket.on("leave-room", (joinedRoom) => {
+        users.delete(socket.id);
+        console.log(users.size);
+        socket.rooms.delete(joinedRoom)
+        console.log(socket.rooms);
+        
+    });
+
+    socket.on('disconnect', () => {
+        users.delete(socket.id);
+        console.log(users.size);
+    });
+   
+});
+
+server.listen(9000, () => {
+    console.log("Server is running");
+});
